8a886207885d70587165d5b1aa9ea8e8e187e530
ccallahaniv <ccallahaniv@gmail.com>
2018-06-24 16:56:47 -0700

    Added django_heroku

--- mentorship/settings.py
+++ mentorship/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,3 +152,5 @@ else:
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = '/login/'
+
+django_heroku.settings(locals())
