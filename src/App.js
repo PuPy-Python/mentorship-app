cf9058e8e0b7c4913ed4c9722926f7fd6bca6f72
brauliojde <bjdelato@ucsc.edu>
2018-08-02 18:22:41 -0700

    Edited App.js with added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './Main/Main';
+import Header from './Main/Header'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
+          <Header/>
           <Main />
         </div>
       </BrowserRouter>
