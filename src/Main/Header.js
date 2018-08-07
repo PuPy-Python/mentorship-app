b293809f7da26e6cc1a3d583e1ef492038498784
Braulio De La Torre <bjdelato@ucsc.edu>
2018-08-07 12:30:01 -0700

    Feature/register (#49)
    * Edited App.js with added Header
    * Added new routes to Main with Mentor Register
    * Header File
    * Register Folder added with MentorRegister js and css
    * forgot to add the export

---
+++ src/Main/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component{
+  render(){
+    return(
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/hello"> Hello Home</Link></li>
+            <li><Link to="/Register/Register"> Register</Link></li>
+          </ul>
+        </nav>
+      </header>
+  );
+        }
+}
+
+export default Header
