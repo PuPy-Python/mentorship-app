f48713d5b51aa91ad3da7b7c376a10d4ac093b01
andriiglukhyi <andriigluxii@gmail.com>
2018-08-17 00:40:09 -0700

    login with local storage

--- src/Main/actions/auth.js
+++ src/Main/actions/auth.js
@@ -1,59 +1,37 @@
-export const login = (username, password) => {
+export const loadUser = () => {
   return (dispatch, getState) => {
-    let headers = {"Content-Type": "application/json"};
-    let body = JSON.stringify({username, password});
+    dispatch({type: "USER_LOADING"});
 
-    return fetch("/api/v1/login/", {headers, body, method: "POST"})
+    const token = getState().auth.token;
+
+    let headers = {
+      "Content-Type": "application/json",
+    };
+
+    if (token) {
+      headers["Authorization"] = `Token ${token}`;
+    }
+    return fetch("api/v1/login/", {headers, })
       .then(res => {
-        if (res.status < 500) {
-          return res.json().then(data => {
-            return {status: res.status, data};
-          })
-        } else {
-          console.log("Server Error!");
-          throw res;
-        }
+        // if (res.status < 500) {
+        //   return res.json().then(data => {
+        //     return {status: res.status, data};
+        //   })
+        // } else {
+        //   console.log("Server Error!");
+        //   throw res;
+        // }
+        console.log(res)
       })
       .then(res => {
-        if (res.status === 200) {
-          dispatch({type: 'LOGIN_SUCCESSFUL', data: res.data });
-          console.log(res.data);
-        } else if (res.status === 403 || res.status === 401) {
-          dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
-          throw res.data;
-        } else {
-          dispatch({type: "LOGIN_FAILED", data: res.data});
-          throw res.data;
-        }
-      })
-  }
-}
-
-export const logout = () => {
-  return (dispatch, getState) => {
-    let headers = {"Content-Type": "application/json"};
-
-    return fetch("api/v1/logout/", {headers, body: "", method: "POST"})
-      .then(res => {
-        if (res.status === 204) {
-          return {status: res.status, data: {}};
-        } else if (res.status < 500) {
-          return res.json().then(data => {
-            return {status: res.status, data};
-          })
-        } else {
-          console.log("Server Error!");
-          throw res;
-        }
-      })
-      .then(res => {
-        if (res.status === 204) {
-          dispatch({type: 'LOGOUT_SUCCESSFUL'});
-          return res.data;
-        } else if (res.status === 403 || res.status === 401) {
-          dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
-          throw res.data;
-        }
+        // if (res.status === 200) {
+        //   dispatch({type: 'USER_LOADED', user: res.data });
+        //   return res.data;
+        // } else if (res.status >= 400 && res.status < 500) {
+        //   dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
+        //   throw res.data;
+        // }
+        console.log('yes almost there')
       })
   }
 }
--- src/Main/reducer/index.js
+++ src/Main/reducer/index.js
@@ -1,3 +1,7 @@
 import auth from "./auth";
+import { combineReducers } from "../../../../../.cache/typescript/2.9/node_modules/redux";
 
-const state = auth
+const mentyApp = combineReducers({
+    auth
+});
+export default mentyApp
