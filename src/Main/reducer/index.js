9d4f47debc1e0604362b3d261be718cb7d5ebf87
andriiglukhyi <andriigluxii@gmail.com>
2018-08-17 12:57:22 -0700

    functionality for jwt

--- src/Main/reducer/index.js
+++
@@ -1,7 +0,0 @@
-import auth from "./auth";
-import { combineReducers } from "../../../../../.cache/typescript/2.9/node_modules/redux";
-
-const mentyApp = combineReducers({
-    auth
-});
-export default mentyApp
