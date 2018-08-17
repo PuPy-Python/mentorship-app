72e143fd817ceb396e4f8e085158a181b7beccab
andriiglukhyi <andriigluxii@gmail.com>
2018-08-17 00:40:09 -0700

    login with local storage

--- src/Main/Login/loginform/login.js
+++ src/Main/Login/loginform/login.js
@@ -1,57 +1,68 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
+import {auth} from "../../actions";
 
 
 class Login extends Component {
 
-  state = {
-    username: "",
-    password: "",
-  }
-
   onSubmit = e => {
     e.preventDefault();
-    console.error("Not implemented!!1");
+    this.props.login(this.state.username, this.state.password);
   }
 
   render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to="/" />
+    }
     return (
       <form onSubmit={this.onSubmit}>
-        <fieldset>
-          <legend>Login</legend>
-          <p>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text" id="username"
-              onChange={e => this.setState({username: e.target.value})} />
-          </p>
-          <p>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password" id="password"
-              onChange={e => this.setState({password: e.target.value})} />
-          </p>
-          <p>
-            <button type="submit">Login</button>
-          </p>
+      <fieldset>
+        <legend>Login</legend>
+        <p>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text" id="username"
+            onChange={e => this.setState({username: e.target.value})} />
+        </p>
+        <p>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password" id="password"
+            onChange={e => this.setState({password: e.target.value})} />
+        </p>
+        <p>
+          <button type="submit">Login</button>
+        </p>
 
-          <p>
-            Don't have an account? <Link to="/register">Register</Link>
-          </p>
-        </fieldset>
-      </form>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </fieldset>
+    </form>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return {};
+  let errors = [];
+  if (state.auth.errors) {
+    errors = Object.keys(state.auth.errors).map(field => {
+      return {field, message: state.auth.errors[field]};
+    });
+  }
+  return {
+    errors,
+    isAuthenticated: state.auth.isAuthenticated
+  };
 }
 
 const mapDispatchToProps = dispatch => {
-  return {};
+  return {
+    login: (username, password) => {
+      return dispatch(auth.login(username, password));
+    }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -6,28 +6,82 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import {auth} from "./actions";
-import state from './reducer'
+import mentyApp from "./reducer";
 
 import Home from './Home/Home';
 import HelloWorld from './HelloWorld';
 import Register from './Register/MentorRegister';
-import Login from './Login/loginform/login'
+import Login from './Login/loginform/login';
+
+let store = createStore(mentyApp, applyMiddleware(thunk));
 
 
-class Main extends Component {
+class RootContainerComponent extends Component {
+
+  componentDidMount() {
+    this.props.loadUser();
+  }
+
+  PrivateRoute = ({component: ChildComponent, ...rest}) => {
+    return <Route {...rest} render={props => {
+      if (this.props.auth.isLoading) {
+        return <em>Loading...</em>;
+      } else if (!this.props.auth.isAuthenticated) {
+        return <Redirect to="/login" />;
+      } else {
+        return <ChildComponent {...props} />
+      }
+    }} />
+  }
   render() {
+    let {PrivateRoute} = this;
     return (
-      <div className="Main">
-        <Switch>
-          <Route exact path="/hello" component={Home} />
-          <Route path="/hello/helloworld" component={HelloWorld} />
-          <Route path ='/hello/register' component={Register}/>
-          <Route path ='/hello/login' component={Login}/>
-          <Redirect to="/hello" />
-        </Switch>
+    <div className="Main">
+    <Switch>
+      <Route path ='/hello/register' component={Register}/>
+      <Route exact path="/hello" component={Home} />
+      <PrivateRoute exact path="/" component={HelloWorld} />
+      <Route exact path ='/hello/login' component={Login}/>
+      <Redirect to="/hello" />
+      </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  }
+}
 
-export default withRouter(Main);
+const mapDispatchToProps = dispatch => {
+  return {
+    loadUser: () => {
+      return dispatch(auth.loadUser());
+    }
+  }
+}
+
+let RootContainer = connect(mapStateToProps, mapDispatchToProps)(RootContainerComponent);
+
+export default class App extends Component {
+  render() {
+    return (
+    <Provider store={store}>
+    <RootContainer />
+    </Provider>
+    )
+  }
+}
+
+
+
+// class Main extends Component {
+//   render() {
+//     return (
+      
+//     );
+//   }
+// }
+
+// export default withRouter(Main);
