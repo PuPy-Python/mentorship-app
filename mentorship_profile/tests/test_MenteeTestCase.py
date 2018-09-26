42e5626e3e228610117bdc2ffd375e6788108cff
Jesse Klein <jklein08@gmail.com>
2018-09-25 18:45:25 -0700

    Add test for mentee areas of guidance addition

--- mentorship_profile/tests/test_MenteeTestCase.py
+++ mentorship_profile/tests/test_MenteeTestCase.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from .test_utilities import UserFactory
-from ..models import Mentee
+from ..models import Mentee, Mentor
 
 
 class MenteeTestCase(TestCase):
@@ -21,3 +21,8 @@ class MenteeTestCase(TestCase):
 
         self.assertEqual(new_mentee.areas_of_guidance, "unknown")
         self.assertEqual(new_mentee.profile, User.objects.first().profile)
+
+    def test_mentee_areas_exceed_max_length(self):
+        actual = len(",".join(map(lambda x: x[0],Mentor.AREAS_OF_INTEREST_AND_GUIDANCE)))
+        max_allowed = next(x for x in Mentee._meta.fields if x.attname == "areas_of_guidance").max_length
+        self.assertLessEqual(actual, max_allowed)
