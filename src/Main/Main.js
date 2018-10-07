a1f07c32ddced500bd666234980d5b63d7c758e3
Tyler Peek (Seattle) <tpeek@us.imshealth.com>
2018-10-07 16:50:28 -0700

    fix route typo

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import Home from './Home/Home';
 import Registration from './Register/Registration';
-import LoginForm from './Login/LoginForm';
+import Login from './containers/Login';
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route path="/signup" component={Registration} />
-          <Route path="/login" component={LoginForm} />
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Redirect to="/" />
         </Switch>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import createHistory from 'history/createBrowserHistory';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import mainTheme from './Main/theme/mainTheme';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
-import Login from './Main/containers/Login';
 import configureStore from './store';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -17,14 +16,11 @@ const store = configureStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <MuiThemeProvider theme={mainTheme}>
-        <Switch>
-          <Route exact path="/login/" component={Login} />
-          <Route path="/" component={App} />
-        </Switch>
-      </MuiThemeProvider>
-    </Router>
+    <MuiThemeProvider theme={mainTheme}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
