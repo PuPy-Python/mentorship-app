3420c063d97619a22a816a9160f29e89e0cc4d9f
Tyler Peek (Seattle) <tpeek@us.imshealth.com>
2018-10-07 12:16:30 -0700

    fix routes

--- src/Main/Header.js
+++ src/Main/Header.js
@@ -1,4 +1,3 @@
-import Menu from '@material-ui/core/Menu';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -12,13 +11,10 @@ class Header extends Component {
               <Link to="/">PuPPy Mentorship</Link>
             </li>
             <li>
-              <Link to="/register">Sign Up</Link>
+              <Link to="/signup">Sign Up</Link>
             </li>
             <li>
               <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/code-of-conduct">Code of Conduct</Link>
             </li>
           </ul>
         </nav>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import Home from './Home/Home';
 import Registration from './Register/Registration';
+import LoginForm from './Login/LoginForm';
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
         <Switch>
-          <Route exact path="/hello" component={Home} />
-          <Route path="/Registration" component={Registration} />
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={Registration} />
+          <Route path="/login" component={LoginForm} />
         </Switch>
-        <Home />
       </div>
     );
   }
