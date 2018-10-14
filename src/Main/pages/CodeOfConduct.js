30c254102db265935263fff6618285da33289539
Tyler Peek (Seattle) <tpeek@us.imshealth.com>
2018-10-14 15:17:44 -0700

    fix lint errors

--- src/Main/pages/CodeOfConduct.js
+++ src/Main/pages/CodeOfConduct.js
@@ -4,54 +4,56 @@ import Typography from '@material-ui/core/Typography';
 export default function CodeOfConduct() {
   return (
     <div>
-      <Typography variant="display3">
-        General Code of Conduct
-      </Typography>
+      <Typography variant="display3">General Code of Conduct</Typography>
       <p>
-        This Code of conduct has been adapted from the
-        <a href="https://www.pspython.com/pages/code-of-conduct/"> PuPPy Code of Conduct
-        </a> as well as the
-        <a href="https://github.com/python/pycon-code-of-conduct/blob/master/code_of_conduct.md"> PyCon Code of Conduct</a>
+        {'This Code of conduct has been adapted from the '}
+        <a
+          href="https://www.pspython.com/pages/code-of-conduct/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {'PuPPy Code of Conduct'}
+        </a>
+        {' as well as the '}
+        <a
+          href="https://github.com/python/pycon-code-of-conduct/blob/master/code_of_conduct.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PyCon Code of Conduct
+        </a>
+        {'.'}
       </p>
 
       <p>
-        PuPPy US is a community local user group intended for networking
-        and collaboration in the developer community.  The PuPPy
-        Mentorship Application was designed to enable development of
-        further connections and relationships between PuPPy community
-        members.  As such, users of this application agree to abide by
-        the PuPPy community code of conduct.
+        PuPPy US is a community local user group intended for networking and collaboration in the
+        developer community. The PuPPy Mentorship Application was designed to enable development of
+        further connections and relationships between PuPPy community members. As such, users of
+        this application agree to abide by the PuPPy community code of conduct.
       </p>
 
       <p>
-        We value the participation of each member of the Python community
-        and want all users of the PuPPy Mentorship App to have an
-        enjoyable and fulfilling experience. Accordingly, all users are
-        expected to show respect and courtesy to other users across the
-        application, and at all local user group events, whether
-        officially sponsored by PuPPy or not.
+        We value the participation of each member of the Python community and want all users of the
+        PuPPy Mentorship App to have an enjoyable and fulfilling experience. Accordingly, all users
+        are expected to show respect and courtesy to other users across the application, and at all
+        local user group events, whether officially sponsored by PuPPy or not.
       </p>
 
       <p>
-        To make clear what is expected, all users, collaborators, and
-        volunteers for this platform are required to conform to the
-        following Code of Conduct. Maintainers will enforce this code
+        To make clear what is expected, all users, collaborators, and volunteers for this platform
+        are required to conform to the following Code of Conduct. Maintainers will enforce this code
         throughout use of this platform.
       </p>
 
       <Typography variant="display1">Our Standards</Typography>
       <p>
-        PuPPy is dedicated to providing a positive experience
-        for everyone, regardless of age, gender identity and expression,
-        sexual orientation, disability, physical appearance, body size,
-        ethnicity, nationality, race, or religion (or lack thereof),
-        education, or socio-economic status.
+        PuPPy is dedicated to providing a positive experience for everyone, regardless of age,
+        gender identity and expression, sexual orientation, disability, physical appearance, body
+        size, ethnicity, nationality, race, or religion (or lack thereof), education, or
+        socio-economic status.
       </p>
 
-      <p>
-        Examples of behavior that contributes to creating a positive
-        environment include:
-      </p>
+      <p>Examples of behavior that contributes to creating a positive environment include:</p>
 
       <ul>
         <li>Being kind to others</li>
@@ -69,25 +71,22 @@ export default function CodeOfConduct() {
         <li>Harassment of other users in any form</li>
         <li>Deliberate intimidation, stalking, or following</li>
         <li>Violent threats or language directed against another person</li>
-        <li>Sexual language and imagery shared in communications with other users
-        </li>
+        <li>Sexual language and imagery shared in communications with other users</li>
         <li>Insults or put downs</li>
-        <li>Sexist, racist, xenophobic, homophobic, transphobic, ableist, or
-          exclusionary jokes</li>
+        <li>Sexist, racist, xenophobic, homophobic, transphobic, ableist, or exclusionary jokes</li>
         <li>Excessive swearing</li>
         <li>Unwelcome sexual attention or advances</li>
         <li>Unwelcome physical contact</li>
         <li>
-          Other conduct that is inappropriate for a professional community group
-          including people of many different backgrounds
+          Other conduct that is inappropriate for a professional community group including people of
+          many different backgrounds
         </li>
       </ul>
 
       <p>
-        If a user engages in behavior that violates this code of conduct, the
-        PuPPy Mentorship App maintainers may take any action they deem
-        appropriate, including warning the offender or expulsion from the
-        application.
+        If a user engages in behavior that violates this code of conduct, the PuPPy Mentorship App
+        maintainers may take any action they deem appropriate, including warning the offender or
+        expulsion from the application.
       </p>
       <Typography variant="display1">Mentor Commitment Guidelines</Typography>
       <p>(Need content here)</p>
